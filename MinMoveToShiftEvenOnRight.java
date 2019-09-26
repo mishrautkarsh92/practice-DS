1f205f3915dfc575c2ad52c4026ffdbea2462de2
utkarshnovborn <utkarshmishra067@gmail.com>
2019-09-26 23:07:58 +0530

    Program to find Minimum move to shift all Even element on right side of an array

---
+++ MinMoveToShiftEvenOnRight.java
@@ -0,0 +1,26 @@
+package com.practice.arrays;
+
+public class MinMoveToShiftEvenOnRight {
+    public static void main(String[] args) {
+        int arr[] = new int[]{};
+        System.out.println(getMinMoveToShiftEvenOnLeft(arr));
+    }
+    public static int getMinMoveToShiftEvenOnLeft(int arr[]){
+        int move = 0;
+        int temp = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2!=0) {
+                for (int j = arr.length - 1; j > i; j--) {
+                    if (arr[j] % 2 == 0){
+                          temp = arr[j];
+                          arr[j] = arr[i];
+                          arr[i] = temp;
+                          move+=1;
+                          break;
+                    }
+                }
+            }
+        }
+        return move;
+    }
+}
